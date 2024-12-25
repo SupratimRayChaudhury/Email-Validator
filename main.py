bd310a6935472c7536cc9673295d9e4cffa79b85
Supratim Ray Chaudhury <116081951+SupratimRayChaudhury@users.noreply.github.com>
2024-12-25 23:50:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 impoet re 
-email_condition="^[a-z]+[\._
+email_condition="^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+user_email=input('Enter your Email : ')
+
+if re.search(email_condition,user_email):
+    print(" Right Email ")
